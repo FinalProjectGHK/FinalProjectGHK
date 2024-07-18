c4791ad4ca47395f91ec8cc2cf0a376735b7532f
Nicholas-Chui <nicholaschui133@gmail.com>
2024-07-18 17:35:49 +0800

    done

--- frontend/src/navbar_routers/Account.js
+++ frontend/src/navbar_routers/Account.js
@@ -1,10 +1,44 @@
 import styles from "./Account.module.css";
+<<<<<<< HEAD
 import { useState } from "react";
 import Headbar from "../components/Headbar";
+=======
+import React, { useState } from "react";
+import Headbar from "../components/Headbar";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import accountPic from "../image/accountPage.png";
+>>>>>>> 16febad (account page)
 
 function Account() {
-    const [inputs, setInputs] = useState({});
+  const [canEdit, setCanEdit] = useState(false);
+  return (
+    <div>
+      <div>
+        <Headbar />
+      </div>
+      <div className={styles.container}>
+        <div className={styles.accountBox}>
+          <form>
+            <h1 style={{ marginBottom: "15px" }}>
+              Customer Profile <AccountBoxIcon />
+            </h1>
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+              <div>
+                <label
+                  for="first_name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
+                  placeholder="John"
+                  required
+                />
+              </div>
 
+<<<<<<< HEAD
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
@@ -51,8 +85,83 @@ function Account() {
                 </div>
             </form> */}
             </div>
+=======
+              <div>
+                <label
+                  for="phone"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Phone number
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
+                  placeholder="123-45-678"
+                  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                  required
+                />
+              </div>
+            </div>
+            <div className="mb-6">
+              <label
+                for="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
+                placeholder="john.doe@company.com"
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                for="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
+                placeholder="•••••••••"
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                for="confirm_password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Confirm password
+              </label>
+              <input
+                type="password"
+                id="confirm_password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
+                placeholder="•••••••••"
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="text-white bg-grey-700 hover:bg-grey-800 focus:ring-4 focus:outline-none focus:ring-grey-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-grey-600 dark:hover:bg-grey-700 dark:focus:ring-grey-800"
+            >
+              Submit
+            </button>
+          </form>
+>>>>>>> 16febad (account page)
         </div>
-    );
+        <img src={accountPic}></img>
+      </div>
+    </div>
+  );
 }
 
 export default Account;
--- frontend/src/navbar_routers/Login.js
+++ frontend/src/navbar_routers/Login.js
@@ -31,37 +31,56 @@ function Login() {
   }
 
   return (
-    <div>
-      <div>
-        <Headbar />
-      </div>
-      <div className={styles.container}>
-        <div className={styles.loginBox}>
-          <form onSubmit={handleSubmit} class="space-y-6" action="#">
-            <h1>
-              Welcome Back
-              <DinnerDiningIcon
-                style={{ marginLeft: "5px" }}
-                fontSize="large"
-              />
-            </h1>
-            <div>
-              <label
-                style={{ display: "flex" }}
-                for="tel"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Your Telephone
-              </label>
+    <div className={styles.container}>
+      <div className={styles.loginBox}>
+        <form onSubmit={handleSubmit} className="space-y-6" action="#">
+          <h1>
+            Welcome Back
+            <DinnerDiningIcon style={{ marginLeft: "5px" }} fontSize="large" />
+          </h1>
+          <div>
+            <label
+              style={{ display: "flex" }}
+              for="tel"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your Telephone
+            </label>
+            <input
+              style={{ width: "250px" }}
+              onChange={handleTelChange}
+              type="text"
+              name="tel"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              required
+            />
+          </div>
+          <div>
+            <label
+              style={{ display: "flex" }}
+              for="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your Password
+            </label>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
               <input
                 style={{ width: "250px" }}
-                onChange={handleTelChange}
-                type="text"
-                name="tel"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                onChange={handlePwChange}
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                 required
               />
             </div>
+<<<<<<< HEAD
             <div>
               <label
                 style={{ display: "flex" }}
@@ -84,12 +103,24 @@ function Login() {
                   name="password"
                   placeholder="••••••••"
                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+=======
+          </div>
+          <div className="flex items-start">
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
+                <input
+                  id="remember"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-gray-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+>>>>>>> 16febad (account page)
                   required
                 />
                 <div onClick={togglePwVis} style={{ marginLeft: "10px" }}>
                   {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
                 </div>
               </div>
+<<<<<<< HEAD
             </div>
             <div class="flex items-start">
               <div class="flex items-start">
@@ -134,6 +165,38 @@ function Login() {
           </form>
         </div>
         <img src={loginPic}></img>
+=======
+              <label
+                for="remember"
+                className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Remember me
+              </label>
+            </div>
+            <a
+              href="#"
+              className="ms-auto text-sm text-gray-700 hover:underline dark:text-gray-500"
+            >
+              Lost Password?
+            </a>
+          </div>
+          <button
+            type="submit"
+            className="w-full text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+          >
+            Login to your account
+          </button>
+          <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+            Not registered?{" "}
+            <a
+              href="#"
+              className="text-gray-700 hover:underline dark:text-gray-500"
+            >
+              Create account
+            </a>
+          </div>
+        </form>
+>>>>>>> 16febad (account page)
       </div>
     </div>
   );
