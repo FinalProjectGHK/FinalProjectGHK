0b4d816ffea8d1cfc4eb39fe0462e20b7b9ade59
Nicholas-Chui <nicholaschui133@gmail.com>
2024-07-19 15:08:22 +0800

    changed account login register

--- frontend/src/navbar_routers/Account.js
+++ frontend/src/navbar_routers/Account.js
@@ -3,9 +3,76 @@ import React, { useState } from "react";
 import Headbar from "../components/Headbar";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import accountPic from "../image/accountPage.png";
+import EditIcon from "@mui/icons-material/Edit";
+import EditOffIcon from "@mui/icons-material/EditOff";
 
 function Account() {
-  const [canEdit, setCanEdit] = useState(false);
+  const [canEditName, setCanEditName] = useState(false);
+  const [canEditTel, setCanEditTel] = useState(false);
+  const [canEditEmail, setCanEditEmail] = useState(false);
+  const [canEditPw, setCanEditPw] = useState(false);
+  const [info, setInfo] = useState({
+    name: "",
+    tel: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  function editName() {
+    setCanEditName((prevState) => {
+      return !prevState;
+    });
+  }
+
+  function editTel() {
+    setCanEditTel((prevState) => {
+      return !prevState;
+    });
+  }
+
+  function editEmail() {
+    setCanEditEmail((prevState) => {
+      return !prevState;
+    });
+  }
+
+  function editPw() {
+    setCanEditPw((prevState) => {
+      return !prevState;
+    });
+  }
+
+  function editNameChange(ev) {
+    setInfo((prevState) => {
+      return { ...prevState, name: ev.target.value };
+    });
+  }
+
+  function editTelChange(ev) {
+    setInfo((prevState) => {
+      return { ...prevState, tel: ev.target.value };
+    });
+  }
+
+  function editEmailChange(ev) {
+    setInfo((prevState) => {
+      return { ...prevState, email: ev.target.value };
+    });
+  }
+
+  function editPwChange(ev) {
+    setInfo((prevState) => {
+      return { ...prevState, password: ev.target.value };
+    });
+  }
+
+  function editCPwChange(ev) {
+    setInfo((prevState) => {
+      return { ...prevState, confirmPassword: ev.target.value };
+    });
+  }
+
   return (
     <div>
       <div>
@@ -20,87 +87,146 @@ function Account() {
             <div className="grid gap-6 mb-6 md:grid-cols-2">
               <div>
                 <label
-                  for="first_name"
+                  style={{ display: "flex" }}
+                  for="name"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Name
                 </label>
-                <input
-                  type="text"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
-                  placeholder="John"
-                  required
-                />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "10px",
+                  }}
+                >
+                  <input
+                    onChange={editNameChange}
+                    value={canEditName ? info["name"] : "Nick"}
+                    type="text"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
+                  />
+                  <div onClick={editName}>
+                    {canEditName ? <EditOffIcon /> : <EditIcon />}
+                  </div>
+                </div>
               </div>
 
               <div>
                 <label
+                  style={{ display: "flex" }}
                   for="phone"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Phone number
                 </label>
-                <input
-                  type="tel"
-                  id="phone"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
-                  placeholder="123-45-678"
-                  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                  required
-                />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "10px",
+                  }}
+                >
+                  <input
+                    onChange={editTelChange}
+                    value={canEditTel ? info["tel"] : "30624700"}
+                    type="tel"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
+                    required
+                  />
+                  <div onClick={editTel}>
+                    {canEditTel ? <EditOffIcon /> : <EditIcon />}
+                  </div>
+                </div>
               </div>
             </div>
             <div className="mb-6">
               <label
+                style={{ display: "flex" }}
                 for="email"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Email address
               </label>
-              <input
-                type="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
-                placeholder="john.doe@company.com"
-                required
-              />
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: "10px",
+                }}
+              >
+                <input
+                  onChange={editEmailChange}
+                  value={canEditEmail ? info["email"] : "nick@gmail.com"}
+                  type="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
+                />
+                <div onClick={editEmail}>
+                  {canEditEmail ? <EditOffIcon /> : <EditIcon />}
+                </div>
+              </div>
             </div>
             <div className="mb-6">
               <label
+                style={{ display: "flex" }}
                 for="password"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Password
               </label>
-              <input
-                type="password"
-                id="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
-                placeholder="•••••••••"
-                required
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                for="confirm_password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: "10px",
+                }}
               >
-                Confirm password
-              </label>
-              <input
-                type="password"
-                id="confirm_password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
-                placeholder="•••••••••"
-                required
-              />
+                <input
+                  onChange={editPwChange}
+                  value={canEditPw ? info["password"] : "hello123456"}
+                  type="text"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
+                />
+                <div onClick={editPw}>
+                  {canEditPw ? <EditOffIcon /> : <EditIcon />}
+                </div>
+              </div>
             </div>
+            {canEditPw ? (
+              <div className="mb-6">
+                <label
+                  style={{ display: "flex" }}
+                  for="confirm_password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Confirm password
+                </label>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "10px",
+                  }}
+                >
+                  <input
+                    onChange={editCPwChange}
+                    type="text"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-grey-500 focus:border-grey-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500"
+                  />
+                </div>
+              </div>
+            ) : null}
 
             <button
               type="submit"
-              className="text-white bg-grey-700 hover:bg-grey-800 focus:ring-4 focus:outline-none focus:ring-grey-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-grey-600 dark:hover:bg-grey-700 dark:focus:ring-grey-800"
+              class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
-              Submit
+              Save
             </button>
           </form>
         </div>
--- frontend/src/navbar_routers/Register.js
+++ frontend/src/navbar_routers/Register.js
@@ -16,6 +16,7 @@ function Register() {
   });
 
   const [pwUnmatch, setPwUnmatch] = useState(false);
+  const [successReg, setSuccessReg] = useState(false);
 
   function handleNameChange(ev) {
     setInput((prevState) => {
@@ -50,8 +51,9 @@ function Register() {
   async function handleSubmit(ev) {
     ev.preventDefault();
     if (input["password"] === input["confirmPassword"]) {
+      setSuccessReg(true);
       setPwUnmatch(false);
-      let result = await fetch("http://localhost:3001/", {
+      let result = await fetch("http://localhost:3001/register", {
         method: "post",
         body: JSON.stringify(input),
         headers: {
@@ -76,134 +78,140 @@ function Register() {
   }
 
   return (
-    <div><Headbar/>
-    <div className={styles.container}>
-      <div className={styles.regBox}>
-        <h1>
-          Become a Member
-          <RememberMeIcon fontSize="large" />
-        </h1>
-        <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
-          <div className="mb-5">
-            <label
-              style={{ display: "flex" }}
-              for="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Name
-            </label>
-            <input
-              value={input["name"]}
-              onChange={handleNameChange}
-              type="text"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
-              required
-            />
-          </div>
-
-          <div className="mb-5">
-            <label
-              style={{ display: "flex" }}
-              for="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Email
-            </label>
-            <input
-              value={input["email"]}
-              onChange={handleEmailChange}
-              type="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
-              required
-            />
-          </div>
-
-          <div className="mb-5">
-            <label
-              style={{ display: "flex" }}
-              for="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Telephone
-            </label>
-            <input
-              value={input["tel"]}
-              onChange={handleTelChange}
-              type="text"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
-              required
-            />
-          </div>
-
-          <div className="mb-5">
-            <label
-              style={{ display: "flex" }}
-              for="password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Password
-            </label>
-            <input
-              value={input["password"]}
-              onChange={handlePwChange}
-              type="password"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
-              required
-            />
-          </div>
-          <div className="mb-5">
-            <label
-              style={{ display: "flex" }}
-              for="repeat-password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Confirm Password
-            </label>
-            <input
-              value={input["confirmPassword"]}
-              onChange={handleConfirmPwChange}
-              type="password"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
-              required
-            />
-          </div>
-
-          <div className="flex items-start mb-5">
-            <div className="flex items-center h-5">
+    <div>
+      <Headbar />
+      <div className={styles.container}>
+        <div className={styles.regBox}>
+          <h1>
+            Become a Member
+            <RememberMeIcon fontSize="large" />
+          </h1>
+          <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
+            <div className="mb-5">
+              <label
+                style={{ display: "flex" }}
+                for="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Name
+              </label>
               <input
-                id="terms"
-                type="checkbox"
-                value=""
-                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-gray-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                value={input["name"]}
+                onChange={handleNameChange}
+                type="text"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
                 required
               />
             </div>
-            <label
-              for="terms"
-              className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              I agree with the{" "}
-              <a
-                href="#"
-                className="text-gray-600 hover:underline dark:text-gray-500"
+
+            <div className="mb-5">
+              <label
+                style={{ display: "flex" }}
+                for="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                terms and conditions
-              </a>
-            </label>
-          </div>
-          <button
-            type="submit"
-            className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-          >
-            Register new account
-          </button>
-        </form>
-        {pwUnmatch ? (
-          <Alert severity="error">Password mismatch, please re-enter.</Alert>
-        ) : null}
+                Email
+              </label>
+              <input
+                value={input["email"]}
+                onChange={handleEmailChange}
+                type="email"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
+                required
+              />
+            </div>
+
+            <div className="mb-5">
+              <label
+                style={{ display: "flex" }}
+                for="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Telephone
+              </label>
+              <input
+                value={input["tel"]}
+                onChange={handleTelChange}
+                type="text"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
+                required
+              />
+            </div>
+
+            <div className="mb-5">
+              <label
+                style={{ display: "flex" }}
+                for="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Password
+              </label>
+              <input
+                value={input["password"]}
+                onChange={handlePwChange}
+                type="password"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                style={{ display: "flex" }}
+                for="repeat-password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Confirm Password
+              </label>
+              <input
+                value={input["confirmPassword"]}
+                onChange={handleConfirmPwChange}
+                type="password"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
+                required
+              />
+            </div>
+
+            <div className="flex items-start mb-5">
+              <div className="flex items-center h-5">
+                <input
+                  id="terms"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-gray-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                  required
+                />
+              </div>
+              <label
+                for="terms"
+                className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                I agree with the{" "}
+                <a
+                  href="#"
+                  className="text-gray-600 hover:underline dark:text-gray-500"
+                >
+                  terms and conditions
+                </a>
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            >
+              Register new account
+            </button>
+          </form>
+          {pwUnmatch ? (
+            <Alert severity="error">Password mismatch, please re-enter.</Alert>
+          ) : null}
+          {successReg ? (
+            <Alert severity="success">
+              Register success, you can now Log in.
+            </Alert>
+          ) : null}
+        </div>
+        <img src={registerPic}></img>
       </div>
-      <img src={registerPic}></img>
-    </div>
     </div>
   );
 }
