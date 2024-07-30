316315be8297ffc8fc537d030c4d3c7cbdc9659a
Nicholas-Chui <nicholaschui133@gmail.com>
2024-07-30 17:05:45 +0800

    latest v

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -2,36 +2,15 @@ import { useState, useEffect, useRef, React } from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 import DrawerComponent from "./components/DrawerComponent";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Avatar from "@mui/material/Avatar";
 import HomeIcon from "@mui/icons-material/Home";
 import { grey } from "@mui/material/colors";
 import { useAuth } from "./components/contexts/AuthContext";
-import Badge from "@mui/material/Badge";
-import { styled } from "@mui/material/styles";
-import IconButton from "@mui/material/IconButton";
 
 const Header = ({ scrollPosition_root, chosenFoods }) => {
   const { currentUser } = useAuth();
   const headerBarRef = useRef();
   const headerBarContentRef = useRef();
-
-  function totalQty() {
-    let count = 0;
-    for (let i = 0; i < chosenFoods.length; i++) {
-      count += chosenFoods[i]["quantity"];
-    }
-    return count;
-  }
-
-  const StyledBadge = styled(Badge)(({ theme }) => ({
-    "& .MuiBadge-badge": {
-      right: -3,
-      top: 0,
-      border: `2px solid ${theme.palette.background.paper}`,
-      padding: "0 4px",
-    },
-  }));
 
   return (
     <div>
@@ -63,13 +42,6 @@ const Header = ({ scrollPosition_root, chosenFoods }) => {
               </Avatar>
             ) : null}
 
-            <Link to="/cart">
-              <IconButton aria-label="cart">
-                <StyledBadge badgeContent={totalQty()} color="primary">
-                  <ShoppingCartIcon sx={{ fontSize: 30 }} />
-                </StyledBadge>
-              </IconButton>
-            </Link>
             <DrawerComponent />
           </div>
         </div>
---
+++ frontend/src/components/RecordList.js
@@ -0,0 +1,77 @@
+// // import styles from "./RecordList.module.css";
+
+// // export default function RecordList({ chineseName, quantity, price }) {
+// //   return (
+// //     <div className={styles.shoppingCartItem}>
+  
+// //       <div className={styles.itemDetails}>
+      
+// //         <div className={styles.itemQuantityPrice}>
+// //         <div className={styles.itemName}>{chineseName}</div>
+// //           <div className={styles.itemQuantity}>
+// //             <span>x{quantity}</span>
+// //           </div>
+// //           <div className={styles.itemPrice}>${price * quantity}</div>
+// //         </div>
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
+// import styles from "./RecordList.module.css";
+
+// export default function RecordList({ chineseName, quantity, price, location, time }) {
+//   return (
+//     <div className={styles.previousOrderRecord}>
+//       <table className={styles.orderTable}>
+//         <thead>
+//           <tr>
+//             <th>Location</th>
+//             <th>Time</th>
+//             <th>Items</th>
+//             <th>Qty</th>
+//             <th>Price</th>
+//             <th>Total Amount</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <td>{location}</td>
+//             <td>{time}</td>
+//             <td>{chineseName}</td>
+//             <td>{quantity}</td>
+//             <td>${price}</td>
+//             <td>${price * quantity}</td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
+
+import React from 'react';
+import styles from './RecordList.module.css';
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+
+const RecordList = ({ orders, onSelectOrder }) => {
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+    return new Date(dateString).toLocaleDateString('en-CA', options).replace(',', '');
+  };
+
+  return (
+    <div className={styles.orderList}>
+      {orders.map((order, index) => (
+        <div key={index} className={styles.orderItem} onClick={() => onSelectOrder(order)}>
+          <div><AccessTimeIcon style={{ color: "#705b38" }} />{" "}{formatDate(order.orderDate)}</div>
+          <div><LocationOnIcon style={{ color: "#705b38" }} />{" "}{order.location}</div>
+          <div><AttachMoneyIcon style={{ color: "#705b38" }} />{" "}{order.totalPrice}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecordList;
